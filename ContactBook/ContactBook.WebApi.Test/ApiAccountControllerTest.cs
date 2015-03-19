b44da94bfa0b19064dd3294372ab41e00ff04054
arif khan <arif.bannehasan@gmail.com>
2015-03-19 09:16:15 -0700

    added webapi test project

---
+++ ContactBook/ContactBook.WebApi.Test/ApiAccountControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactBook.Domain.Models;
+using ContactBook.WebApi.Controllers;
+using Xunit;
+
+namespace ContactBook.WebApi.Test
+{
+   public class ApiAccountControllerTest
+    {
+       [Fact]
+       public void GetUserInfoTest()
+       {
+           var controller = new ApiAccountController();
+           UserInfoViewModel model = controller.GetUserInfo();
+           Assert.True(model != null, "UserInfoViewModel shouldn't be null");
+       }
+       
+    }
+}
---
+++ ContactBook/ContactBook.WebApi.Test/SampleControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Results;
+using ContactBook.WebApi.Controllers;
+using Xunit;
+
+namespace ContactBook.WebApi.Test
+{
+    public class SampleControllerTest
+    {
+        [Fact]
+        public void GetTest()
+        {
+            //prepare
+            var fact = new List<string> { "value1", "value2" };
+            var sampleCntr = new SampleController();
+            //act
+            OkNegotiatedContentResult<string[]> actual = sampleCntr.Get() as OkNegotiatedContentResult<string[]>;
+
+            //assert
+            Assert.True(fact.SequenceEqual(actual.Content), "collections equal");
+        }
+    }
+}
