52ed17f6deae65ef6d420aa5df5217c9a46ec5e3
arif-bannehasan <arif.bannehasan@gmail.com>
2015-09-12 17:20:42 +0530

    new layout has been setup for contact list page

--- ContactBook/ContactBook.Web/app/controllers/ContactsController/newContactCntrl.js
+++ ContactBook/ContactBook.Web/app/controllers/ContactsController/newContactCntrl.js
@@ -1,54 +1,32 @@
 ﻿(function () {
     'use strict';
-    cbControllers.controller('newContactCntrl', ['$scope', '$filter', function ($scope, $filter) {
-        $scope.serverObject = [
-        {
-            name: 'arif khan',
-            phone: '9768836054',
-            email: 'arif788@gmail.com'
-        },
-        {
-            name: 'tarik khan',
-            phone: '1111111111',
-            email: 'tarik@gmail.com'
-        },
-                {
-                    name: 'afreen khan',
-                    phone: '2222222222',
-                    email: 'afreen@gmail.com'
-                },
+    cbControllers.controller('newContactCntrl', ['$scope', '$filter', '$log', 'contactsSvc', function ($scope, $filter, $log, contactsSvc) {
+        $scope.serverObject = null;
+        var contactPromise = contactsSvc.GetContacts();
+        contactPromise.then(function (data) {
+            $scope.contactList = data.data;
+             //init($scope.serverObject);
+        }, function (err) {
+            $log.error('error had occoured');
+        });
 
-                {
-                    name: 'asif sayyed',
-                    phone: '33333333333',
-                    email: 'asif@aol.com'
-                },
-                        {
-                            name: 'sandeep kamat',
-                            phone: '44444444444',
-                            email: 'sandeep@gmail.com'
-                        },
-                                {
-                                    name: 'kamal relwani',
-                                    phone: '5555555555',
-                                    email: 'tarik@gmail.com'
-                                }
-        ]
         $scope.search = '';
+
         $scope.contactList = new Array();
+
         var init = function (objs) {
-            var groupSize = 3;
-            $scope.contactList = _.map(objs, function (item, index) {
-                return index % groupSize === 0 ? objs.slice(index, index + groupSize) : null;
-            }).filter(function (item) {
-                return item;
-            });
+            //var groupSize = 3;
+            //$scope.contactList = _.map(objs, function (item, index) {
+            //    return index % groupSize === 0 ? objs.slice(index, index + groupSize) : null;
+            //}).filter(function (item) {
+            //    return item;
+            //});
         };
+
         $scope.changeObj = function (search) {
             var newArray = $filter('filter')($scope.serverObject, search);
             init(newArray);
         };
 
-        init($scope.serverObject);
     }]);
 })();
---
+++ ContactBook/ContactBook.Web/app/services/contactsSvc.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use stricts';
+
+    cbServices.factory("contactsSvc", ['$http', '$q', 'cbSettings', function ($http, $q, cbSettings) {
+        var getContacts = function () {
+            var deferred = $q.defer();
+
+            $http.get("http://contactbook.com/contactbook.web/app/data/contactslist.json").then(function (data) {
+                deferred.resolve(data);
+            });
+            return deferred.promise;
+        };
+
+        return {
+            GetContacts: getContacts
+        };
+    }]);
+
+})();
