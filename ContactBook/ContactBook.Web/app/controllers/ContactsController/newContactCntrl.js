cbb59d0b49ce25f5ef23bcfc1195c17c0b01b549
arif_bannehasan1 <arif.bannehasan@gmail.com>
2015-09-24 14:44:05 +0000

    changes made to the contact book layout

--- ContactBook/ContactBook.Web/app/controllers/ContactsController/newContactCntrl.js
+++ ContactBook/ContactBook.Web/app/controllers/ContactsController/newContactCntrl.js
@@ -1,32 +1,74 @@
-﻿(function () {
+(function() {
     'use strict';
-    cbControllers.controller('newContactCntrl', ['$scope', '$filter', '$log', 'contactsSvc', function ($scope, $filter, $log, contactsSvc) {
-        $scope.serverObject = null;
-        var contactPromise = contactsSvc.GetContacts();
-        contactPromise.then(function (data) {
-            $scope.contactList = data.data;
-             //init($scope.serverObject);
-        }, function (err) {
-            $log.error('error had occoured');
-        });
+    cbControllers.controller('newContactCntrl', ['$scope', '$filter', '$log', 'contactsSvc', function($scope, $filter, $log, contactsSvc) {
+        var serverObjects = [];
 
         $scope.search = '';
+        $scope.letterList = [];
+        $scope.selectedLetter = '*';
 
-        $scope.contactList = new Array();
-
-        var init = function (objs) {
-            //var groupSize = 3;
-            //$scope.contactList = _.map(objs, function (item, index) {
-            //    return index % groupSize === 0 ? objs.slice(index, index + groupSize) : null;
-            //}).filter(function (item) {
-            //    return item;
-            //});
+        //Pagination variables
+        $scope.totalItems = 0;
+        $scope.currentPage = 1;
+        $scope.maxSize = 10;
+        $scope.itemPerPage = 12;
+        
+        /*
+         * init method make service call to get all the contacts
+         */
+        var init = function() {
+            populateLetters();
+            contactsSvc.GetContacts().then(onSuccess, onError);
         };
 
-        $scope.changeObj = function (search) {
-            var newArray = $filter('filter')($scope.serverObject, search);
-            init(newArray);
+        var onSuccess = function(data) {
+            serverObjects = angular.copy(data.data);
+            
+            if (angular.isDefined(serverObjects)) {
+                populateContacts(serverObjects);
+            }
         };
 
+        var onError = function(err) {
+            $log.error('error had occoured');
+        };
+
+        var populateContacts = function(objs) {
+            $scope.contactList = [];
+            $scope.pagedContactList = [];
+            
+            angular.forEach(objs, function(value, key) {
+                $scope.contactList.push(value);
+            });
+            
+            $scope.totalItems = $scope.contactList.length;
+            $scope.currentPage = 1;
+            $scope.pagedContactList = _.chunk($scope.contactList, $scope.itemPerPage)[$scope.currentPage - 1];
+        };
+
+        var populateLetters = function() {
+            $scope.letterList.push('*');
+            for (var i = 65; i < 91; i++) {
+                $scope.letterList.push(String.fromCharCode(i));
+            }
+        };
+
+        $scope.searchContact = function(search) {
+            var newArray = $filter('filter')(serverObjects, search);
+            populateContacts(newArray);
+            $scope.selectedLetter = $scope.letterList[0];
+        };
+
+        $scope.searchByLetter = function(letter) {
+            var newArray = $filter('letterFilter')(serverObjects, letter);
+            populateContacts(newArray);
+            $scope.selectedLetter = letter;
+        }
+
+        $scope.pageChanged = function() {
+            $scope.pagedContactList = _.chunk($scope.contactList, $scope.itemPerPage)[$scope.currentPage - 1];
+        };
+
+        init();
     }]);
 })();
--- ContactBook/ContactBook.Web/app/services/contactsSvc.js
+++ ContactBook/ContactBook.Web/app/services/contactsSvc.js
@@ -2,15 +2,17 @@
     'use stricts';
 
     cbServices.factory("contactsSvc", ['$http', '$q', 'cbSettings', function ($http, $q, cbSettings) {
+        
         var getContacts = function () {
             var deferred = $q.defer();
 
-            $http.get("http://contactbook.com/contactbook.web/app/data/contactslist.json").then(function (data) {
+            $http.get("http://contactbook-arif-bannehasan1.c9.io/ContactBook/ContactBook.Web/app/data/contactslist.json?_c9_id=livepreview0&_c9_host=https://ide.c9.io#/newContact").then(function (data) {
                 deferred.resolve(data);
             });
             return deferred.promise;
         };
-
+        
+        
         return {
             GetContacts: getContacts
         };
