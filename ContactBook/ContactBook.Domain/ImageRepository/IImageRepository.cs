9f6cf4678de8525c646718a105a952a02199ee04
arif.bannehasan1 <arif.bannehasan@gmail.com>
2016-03-14 00:07:11 +0530

    Adding untraceked files

---
+++ ContactBook/ContactBook.Domain/ImageRepository/IImageRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ContactBook.Domain.ImageRepository
+{
+    public interface IImageRepository
+    {
+        void SaveAsync(string fileName, Stream stream);
+        List<string> GetFileNames();
+        ImageFileData GetFileData(string _fileName);
+        string UserName { get; set; }
+    }
+}
